2225d5309477603cc9edd2630a1d83bffdfba396
craciu25yt <48807342+craciu25yt@users.noreply.github.com>
2026-07-02 00:31:23 +0200

    bypass gh pages cache via rawusercontent

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => ({
 			userscript: {
 				name: "Genie",
 				namespace: "npm/lyeh",
-				// I swear I hate live server
 				version: "1.1.0",
 				match: ["https://genius.com/*"],
 				exclude: ["*://assets.genius.com/javascripts/compiled/apple_music*"],
@@ -23,9 +22,8 @@ export default defineConfig(({ command }) => ({
 				],
 				connect: ["genius.com", "images.genius.com", "t2.genius.com"],
 				"run-at": "document-start",
-				// lost 30 minutes because of this
-				updateURL: "https://craciu25yt.github.io/lyeh/genie.user.js",
-				downloadURL: "https://craciu25yt.github.io/lyeh/genie.user.js",
+				updateURL: "https://raw.githubusercontent.com/craciu25yt/lyeh/gh-pages/genie.user.js",
+				downloadURL: "https://raw.githubusercontent.com/craciu25yt/lyeh/gh-pages/genie.user.js",
 			},
 		}),
 		vue(),
